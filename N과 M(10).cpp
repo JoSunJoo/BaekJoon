e1faad6b435f7b9d0b1fcdbed747508876f1b5e6
JoSunJoo <49556084+JoSunJoo@users.noreply.github.com>
2020-02-01 20:25:27 +0900

    15664번

---
+++ N과 M(10).cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<algorithm>
+using namespace std;
+int N, M; //입력갯수, cnt개수
+int arr[10];
+int res[10];
+
+int visited[10] = {0};
+void DFS(int len, int p) {
+	if (len == M) {
+		for (int i = 0; i < len; i++) printf("%d ", res[i]);
+		printf("\n");
+		return;
+	}
+	int check[10001] = {0};
+	for (int i = p; i < N; i++) {
+		if (visited[i] == 0 && check[arr[i]] == 0) {
+			visited[i] = 1;
+			check[arr[i]] = 1;
+			res[len] = arr[i];
+			DFS(len + 1, i);
+			visited[i] = 0;
+		}
+	}
+}
+
+int main() {
+	scanf("%d %d", &N, &M);
+	for (int i = 0; i < N; i++) scanf("%d", &arr[i]);
+	sort(arr, arr+N);
+	DFS(0, 0);
+	return 0;
+}
