27c548604c3b5ae2fc06b78f3a355f170244996b
JoSunJoo <49556084+JoSunJoo@users.noreply.github.com>
2020-02-27 21:28:51 +0900

    10992번

---
+++ 별 찍기-17.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+	int N;	scanf("%d", &N);
+	for (int i = 1; i < N; i++) printf(" ");
+	printf("*\n");
+	for (int i = 2; i < N; i++) {
+		for (int j = N; j > i; j--) printf(" ");
+		printf("*");
+		for (int j = 1; j <= 2 * i - 3; j++) printf(" ");
+		printf("*\n");
+	}
+	if (N > 1) {
+		for (int i = 1; i <= 2 * N - 1; i++) printf("*");
+	}
+}
