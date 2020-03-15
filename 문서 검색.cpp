99386f503125f38df2f7b51d5e158dd7230ac0dc
JoSunJoo <49556084+JoSunJoo@users.noreply.github.com>
2020-03-15 23:57:48 +0900

    1543번

---
+++ 문서 검색.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string S, subS;
+int main(void){
+	getline(cin, S);
+	getline(cin, subS);
+		
+	if (subS.size() > S.size())	cout << 0 << "\n";
+	else{
+		int result = 0;
+		for (int i = 0; i < S.size() - subS.size() + 1; i++){
+			bool same = true;
+			for (int j = 0; j < subS.size(); j++)
+				if (S[i + j] != subS[j]){
+					same = false;
+					break;
+				}
+			if (same){
+				result++;
+				i += subS.size() - 1;
+			}
+		}
+		cout << result << "\n";
+	}
+	return 0;
+}
