645252bf1748a2560f1a8e5ec2b0ce2243c3ccee
JoSunJoo <49556084+JoSunJoo@users.noreply.github.com>
2020-04-28 04:29:21 +0900

    1543번

--- 문서 검색.cpp
+++ 문서 검색.cpp
@@ -1,28 +1,31 @@
-#include <iostream>
-#include <string>
+#include<stdio.h>
+#include<iostream>
+#include<string>
 using namespace std;
 
-string S, subS;
-int main(void){
-	getline(cin, S);
-	getline(cin, subS);
-		
-	if (subS.size() > S.size())	cout << 0 << "\n";
-	else{
-		int result = 0;
-		for (int i = 0; i < S.size() - subS.size() + 1; i++){
-			bool same = true;
-			for (int j = 0; j < subS.size(); j++)
-				if (S[i + j] != subS[j]){
-					same = false;
-					break;
-				}
-			if (same){
-				result++;
-				i += subS.size() - 1;
-			}
-		}
-		cout << result << "\n";
+string input, token;
+bool check(int point) {
+	int p = 0;
+	for (int i = point; i < point + token.size(); i++) {
+		if (input[i] != token[p++])	return false;
 	}
+	return true;
+}
+
+int main() {
+	getline(cin, input);
+	getline(cin, token);
+	if (input.size() < token.size()) {
+		printf("0");
+		return 0;
+	}
+	int cnt = 0;
+	for (int i = 0; i < input.size()-token.size()+1; i++) {
+		if (input[i] == token[0] && check(i)) {
+			cnt++;
+			i += (token.size()-1);
+		}
+	}
+	printf("%d", cnt);
 	return 0;
 }
